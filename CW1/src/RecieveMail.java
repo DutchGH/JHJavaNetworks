3620bc4666fb95011c3fa98277c122422e6cbc0d
Jacob Holland <sc15j3h@leeds.ac.uk>
2017-02-23 13:03:02 +0000

    Creating Class Structure

---
+++ CW1/src/RecieveMail.java
@@ -0,0 +1,5 @@
+/**
+ * Created by jake on 23/02/2017.
+ */
+public class RecieveMail {
+}
---
+++ CW1/src/SendMail.java
@@ -0,0 +1,9 @@
+/**
+ * Created by jake on 23/02/2017.
+ */
+public class SendMail {
+
+    @Override
+    public String toString() {
+    }
+}
